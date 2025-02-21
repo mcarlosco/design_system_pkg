f3351ea748c014e2f4e09de204297f936079b828
Carlos Murillo <m.carlos.co@gmail.com>
2025-02-21 13:15:47 -0500

    Update validator_util.dart

--- lib/src/utils/validators_util.dart
+++ lib/src/utils/validators_util.dart
@@ -1,5 +1,5 @@
 /// Validates required but nullable [value].
-String? validateNonNull(Object? value) => value == null ? 'Requerdio' : null;
+String? validateNonNull(Object? value) => value == null ? 'Requerido' : null;
 
 /// Validates required but nullable or empty [value].
 String? validateNonEmpty(String? value) =>
